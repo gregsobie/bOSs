496ac6946130298161bc3a991f8e2ac9b0d5c991
mnalam2 <mnalam2@0df279af-beec-47f8-8c4d-85a890efaaa8>
2016-10-16 23:18:52 +0000

    RTC Default Rate Added

--- student-distrib/RTC.c
+++ student-distrib/RTC.c
@@ -2,9 +2,17 @@
 
 void RTC_init()
 {
+	// Initialize rate variable
+	unsigned char rate;
+	// Initialize 2 Hz frequency by setting rate to 15
+	// frequency = 32768 >> (rate-1)
+	// 			 = 32768 >> (15-1) = 2 Hz
+	rate = RTC_DEFAULT_RATE;
+
 	// Disable interrupts
 	cli();
 
+	/* Turn on IRQ 8 */
 	// Disable NMI and select Register B
 	outb(REGISTER_B, NMI);
 	// Read value of Register B
@@ -13,6 +21,16 @@ void RTC_init()
 	outb(REGISTER_B, NMI);
 	// Turn on bit 6 of Register B
 	outb(CMOS, val | MASKBIT6);
+
+	/* Set Default Interrupt Rate */
+	// Disable NMI and select Register A
+	outb(REGISTER_A, NMI);
+	// Read value of Register A
+	val = inb(CMOS);
+	// Set index
+	outb(REGISTER_A, NMI);
+	// Write rate into Register A (bottom 4 bits)
+	outb((val & 0xF0) | rate, CMOS);
 
 	// Enable interrupts
 	sti();
