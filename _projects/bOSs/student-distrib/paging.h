91f8334d949606420b3c0583e98529b5972e5482
bbogue2 <bbogue2@0df279af-beec-47f8-8c4d-85a890efaaa8>
2016-11-13 01:29:29 +0000

    Execute half-done

--- _projects/bOSs/student-distrib/paging.h
+++ _projects/bOSs/student-distrib/paging.h
@@ -1,13 +1,10 @@
-#ifndef _PAGING_H
-#define _PAGING_H
+#ifndef PAGING_H
+#define PAGING_H
 
-extern void loadPageDirectory(unsigned int*);
-extern void enablePaging();
+#include "types.h"
 
-void init_kernel_pd();
-
-uint32_t kernel_page_directory[1024] __attribute__((aligned(4096)));
-uint32_t video_page_table[1024] __attribute__((aligned(4096)));
+#define USER_PROG_LOCATION 0x08048000 
+#define MAX_USER_PROG      0x06
 
 #define FLAG_PRESENT       0x01
 #define FLAG_WRITE_ENABLE  0x02
@@ -19,5 +16,16 @@ uint32_t video_page_table[1024] __attribute__((aligned(4096)));
 #define FLAG_GLOBAL        0x100
 #define TABLE_SIZE         1024
 #define DIRECTORY_SIZE     1024
+
+extern void loadPageDirectory(uint32_t *);
+extern void enablePaging();
+
+void init_kernel_pd();
+
+uint32_t kernel_page_directory[1024] __attribute__((aligned(4096)));
+uint32_t video_page_table[1024] __attribute__((aligned(4096)));
+uint32_t proc_page_directory[MAX_USER_PROG][1024]__attribute__((aligned(4096)));
+static uint8_t proc_id_used[MAX_USER_PROG] = {false};
+
 
 #endif
--- _projects/bOSs/student-distrib/syscall.c
+++ _projects/bOSs/student-distrib/syscall.c
@@ -1,32 +1,113 @@
 #include "syscall.h"
 #include "filesystem.h"
+#include "paging.h"
+#include "x86_desc.h"
 #include "lib.h"
-asmlinkage int32_t halt (uint8_t status){
-	printf("halt\n");
-	//Change paging back
-	//Change TSS
-	//change esp/ebp
-	//close any FDs that need it
-	//jump to halt_ret_label in exec
-	//goto halt_ret_label;
-	return 0;
-}
+
 asmlinkage int32_t execute (const uint8_t* command){
 	printf("exec\n");
 	//Parse args
-	/*dentry_t d;
-	read_dentry_by_name (command, &d);
+	dentry_t d;
+	if(read_dentry_by_name (command, &d) == -1)
+		return -1;
 	uint8_t head[40];
 	read_data (d.inode, 0, head, 40);
 	if(head[0] != 0x7f || head[1] != 0x45 || head[2] != 0x4c || head[3] != 0x46){
 		return -1;
 	}
-	//while we're here, grab the EIP (?endian)
+	//Convert EIP from little endian to big endian
+	//Could use x86 bswap %reg
+	uint32_t eip = (head[27] << 24) | (head[26] << 16) | (head[25] << 8) | head[24];
+	//obtain perm level from flags register?
+	//Obtain PID
+	int i;
+	uint32_t pid = -1;
+	for(i=0;i<MAX_USER_PROG;i++){
+		if(proc_id_used[i] == false){
+			pid = i;
+			proc_id_used[i] = true;
+			break;
+		}
+	}
+	if(pid == -1){
+		printf("Max processes used.");
+		return -1;
+	}
 	//Change paging
+	proc_page_directory[pid][0] =  (uint32_t)video_page_table | FLAG_WRITE_ENABLE | FLAG_PRESENT;
+	proc_page_directory[pid][1] = 0x00400000 | FLAG_4MB_PAGE | FLAG_WRITE_ENABLE | FLAG_PRESENT | FLAG_GLOBAL;
+	proc_page_directory[pid][USER_PROG_LOCATION >> 22] = (0x00400000 * (pid+1)) | FLAG_4MB_PAGE | FLAG_WRITE_ENABLE | FLAG_PRESENT |FLAG_USER;
+	loadPageDirectory(proc_page_directory[pid]);
 	//Load program into program area
-	halt_ret_label:*/
+	read_data (d.inode, 0, (uint8_t *)USER_PROG_LOCATION, 0x400000); //Copy up to 4MB of program data (stack will kill some of it)
+	//Create PCB
+	printf("1\n");
+	PCB_t * pcb = (PCB_t *)(0x800000-0x2000 * (pid+1));
+	pcb->pid = pid;
+	pcb->esp0 = 0x800000-0x2000 * pid -4; //Bottom of stack
+	PCB_t * parent;
+	cur_pcb(parent);
+	pcb->parent = parent;
+	asm volatile("\
+		movl	%%esp,%0 	\n\
+		movl    %%ebp, %1"
+		: "=r"(parent->esp), "=r"(parent->ebp)
+		: 
+		:"memory");
+	uint32_t u_esp = USER_PROG_LOCATION + 0x00400000 -4 ;
+	//init 
+	//pcb.fd[0].file_operations = 
+	//
+
+	//Change TSS
+	tss.esp0 = pcb->esp0;//address of new kernel stack
+	//set up fake table thingy on stack
+	printf("9\n");
+	asm volatile("\
+		cli	\n\
+		movw	%2,%%ax 	\n\
+		movw    %%ax, %%ds		\n\
+		movw    %%ax, %%es		\n\
+		movw    %%ax, %%fs		\n\
+		movw    %%ax, %%gs		\n\
+		movl	%%esp,%%eax		\n\
+		pushl	%2		\n\
+		pushl 	%1			\n\
+		pushf					\n\
+		orl $0x200,(%%esp)	# set the interrupt flag\n\
+		pushl	%3		\n\
+		pushl	%0				\n\
+		iret	\n\
+		halt_ret_label:"
+		:
+		: "r"(eip),"r"(u_esp),"i"(USER_DS),"i"(USER_CS)
+		: "eax");
+	//
 	return 0;
 }
+asmlinkage int32_t halt (uint8_t status){
+	printf("halt\n");
+	PCB_t * pcb;
+	cur_pcb(pcb);
+
+	//Change paging back
+	//Security-shuld clean old memory space
+	loadPageDirectory(proc_page_directory[pcb->parent->pid]);
+	//Change TSS
+	//change esp/ebp
+	//close any FDs that need it
+	//jump to halt_ret_label in exec
+	
+	/*asm volatile("\
+		movl	%0,%%esp 	\n\
+		movl    %1,%%ebp,"	
+		:
+		: "r"(pcb->parent->esp),"r"(pcb->parent->ebp) 
+		: "memory" );*/
+	//goto halt_ret_label;
+	return 0;
+}
+
 asmlinkage int32_t read (int32_t fd, void* buf, int32_t nbytes){
 	printf("read\n");
 	return 0;
